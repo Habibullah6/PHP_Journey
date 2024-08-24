933a18727597b4cf156ea2dab9db52073abdda40
habibullah6 <nomanhabibullah65@gmail.com>
2024-08-24 13:52:59 +0600

    done

--- variable.php
+++ variable.php
@@ -1,4 +1,18 @@
 <?php
 
+// variable
 
+$name = "John";
+$age = 30;
+echo $name . " is " . $age . " years old.";
+echo "\n";
+echo "\n";
+
+
+// constant variable
+
+define("SITE_NAME", "LeadMovie");
+define("MAX_USERS", 100);
+
+echo SITE_NAME;
 
