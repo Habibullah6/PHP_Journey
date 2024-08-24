c4d9bfea4212fb28c1b1762c76f6381a5834c1b9
habibullah6 <nomanhabibullah65@gmail.com>
2024-08-24 15:33:49 +0600

    done

---
+++ arithmetic.php
@@ -0,0 +1,31 @@
+<?php
+// Variables
+$num1 = 10;
+$num2 = 5;
+
+// Addition
+$addition = $num1 + $num2;
+echo "Addition: " . $num1 . " + " . $num2 . " = " . $addition . "\n";
+
+// Subtraction
+$subtraction = $num1 - $num2;
+echo "Subtraction: " . $num1 . " - " . $num2 . " = " . $subtraction . "\n";
+
+// Multiplication
+$multiplication = $num1 * $num2;
+echo "Multiplication: " . $num1 . " * " . $num2 . " = " . $multiplication . "\n";
+
+// Division
+$division = $num1 / $num2;
+echo "Division: " . $num1 . " / " . $num2 . " = " . $division . "\n";
+
+// Addition, subtraction, multiplication, division
+
+// BODMAS = "Bracket or division or multiplication or addition or subtraction"
+
+$number = 28;
+
+// $number  += 2;
+
+
+echo $number;
