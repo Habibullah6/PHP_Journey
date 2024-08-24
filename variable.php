4643fdcdf34e6b8bca8d6bec6b137fee4daa5574
habibullah6 <nomanhabibullah65@gmail.com>
2024-08-24 14:28:56 +0600

    done

--- variable.php
+++ variable.php
@@ -14,5 +14,27 @@ echo "\n";
 define("SITE_NAME", "LeadMovie");
 define("MAX_USERS", 100);
 
-echo SITE_NAME;
+// echo SITE_NAME;
 
+$number = 10;
+$text = "Hello, World!";
+$array = array(1, 2, 3);
+
+var_dump($number);
+var_dump($text);
+var_dump($array);
+
+// ----------------------------------------------
+
+$myDetails = "I am
+
+Habibullah. I am from
+
+Bangladesh";
+
+echo $myDetails;
+
+$name = "Habibullah";
+$age = 26;
+
+printf("my %sname is %s. I am %s year old", 'full', $name, $age);
