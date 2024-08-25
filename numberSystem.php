76a6a223389546e08a3b1173ae5764b498ac0e6c
habibullah6 <nomanhabibullah65@gmail.com>
2024-08-25 14:08:43 +0600

    done

---
+++ numberSystem.php
@@ -0,0 +1,20 @@
+<?php
+$decimal = 15;
+
+// Convert decimal to binary
+$binary = decbin($decimal);
+echo "Decimal $decimal to Binary: $binary\n"; // Output: 1111
+
+// Convert decimal to octal
+$octal = decoct($decimal);
+echo "Decimal $decimal to Octal: $octal\n"; // Output: 17
+
+// Convert decimal to hexadecimal
+$hexadecimal = dechex($decimal);
+echo "Decimal $decimal to Hexadecimal: $hexadecimal\n"; // Output: f
+
+
+
+
+
+
