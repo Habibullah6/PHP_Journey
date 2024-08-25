4e560d3b9e5061edb7cf4176f7732df56ef3e010
habibullah6 <nomanhabibullah65@gmail.com>
2024-08-25 10:42:10 +0600

    done

---
+++ operator.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// Initialize variable
+$number = 10;
+
+// Post-increment
+echo $number++; // Outputs: 10 (original value)
+echo "<br>";
+echo $number;   // Outputs: 11 (incremented value)
+
+// Pre-increment
+echo ++$number; // Outputs: 12 (incremented value)
+echo "<br>";
+
+// Post-decrement
+echo $number--; // Outputs: 12 (original value)
+echo "<br>";
+echo $number;   // Outputs: 11 (decremented value)
+
+// Pre-decrement
+echo --$number; // Outputs: 10 (decremented value)
+
